3b3e74cd1b15529f36115b09cc958dcac3e7649e
Florian Hoffmann <florian.hoffmann@mailbox.org>
2023-11-30 12:41:04 +0100

    added pokemon card board and improved design

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bgColors = [
+const typeColors = [
   { type: "Normal", color: "#A8A77A" },
   { type: "Fire", color: "#EE8130" },
   { type: "Water", color: "#6390F0" },
@@ -18,24 +18,26 @@ const bgColors = [
   { type: "Steel", color: "#B7B7CE" },
 ];
 
-let currentPokemon = "charizard";
+let currentPokemon = 1;
 let currentPokemonData = [];
+let numberOfPokemon = 20;
 let allPokemonData = [];
+let scrollPos = "";
 
 async function init() {
   if (localStorage.getItem("pokemonData")) {
     allPokemonData = await loadFromLocalStorage("pokemonData");
-  } else await loadNumberOfPokemons(151);
+  } else await loadNumberOfPokemons();
 
-  renderPokemonCard();
+  renderPreviewCard();
 }
 
-async function loadNumberOfPokemons(number) {
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=" + number;
+async function loadNumberOfPokemons() {
+  let apiUrl = `https://pokeapi.co/api/v2/pokemon/?limit=${numberOfPokemon}`;
   let apiResponse = await fetch(apiUrl);
   let apiResponseAsJson = await apiResponse.json();
 
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i < numberOfPokemon; i++) {
     let name = apiResponseAsJson["results"][i]["name"];
     await loadPokemonData(name);
   }
@@ -64,64 +66,164 @@ async function loadPokemonData(name) {
   });
 }
 
-function renderPokemonCard() {
-  let index = allPokemonData.findIndex((i) => i.name == currentPokemon);
+function renderPreviewCard() {
+  document.getElementById("board-container").innerHTML = "";
+
+  for (let i = 0; i < allPokemonData.length; i++) {
+    const element = allPokemonData[i];
+    const pokemonName = upperCase(element["name"]);
+    const pokemonId = String(element["id"]);
+    const pokemonImg = element["img"];
+
+    document.getElementById("board-container").innerHTML += `
+      <div id="small-card${i}" class="small-card" onclick="showPokemonCard(${
+      element["id"]
+    })">
+        <div class="d-flex space-between">
+          <h3>${pokemonName}</h3>
+          <span>#${pokemonId.padStart(3, "0")}</span>
+        </div>
+        <div id="small-card-type-container${i}" class="small-card-type-container"> </div>
+        <img src="${pokemonImg}" />
+       </div>
+    
+    `;
+    renderTypes("small-card-type-container" + i, element);
+    setBgColor("small-card" + i, element);
+  }
+}
+
+function showPokemonCard(pokemonId) {
+  scrollPos = window.scrollY;
+  currentPokemon = pokemonId;
+  let index = allPokemonData.findIndex((i) => i.id == currentPokemon);
   currentPokemonData = allPokemonData[index];
 
   renderCardHeader();
-  renderCardAbout();
-  renderCardStats();
+  switchToCardStats();
+  disableButtons();
+
+  document.getElementById("large-card-bg").classList.remove("d-none");
+  document.getElementById("large-card-bg").style.top = window.scrollY + "px";
+  document.body.style.overflow = "hidden";
+}
+
+function hidePokemonCard() {
+  document.getElementById("large-card-bg").classList.add("d-none");
+  window.scrollTo(0, scrollPos);
+  document.body.style.overflow = "auto";
 }
 
 function renderCardHeader() {
-  let pokemonName = capitalize(currentPokemonData["name"]);
-  let pokemonId = String(currentPokemonData['id']);
+  let pokemonName = upperCase(currentPokemonData["name"]);
+  let pokemonId = String(currentPokemonData["id"]);
   let pokemonImg = currentPokemonData["img"];
 
   document.getElementById("pokemon-name").innerHTML = pokemonName;
-  document.getElementById("pokemon-id").innerHTML = "Pokedex #" + pokemonId.padStart(3, "0");
+  document.getElementById("pokemon-id").innerHTML =
+    "Pokedex #" + pokemonId.padStart(3, "0");
   document.getElementById("pokemon-img").src = pokemonImg;
+  setBgColor("large-card", currentPokemonData);
+  renderColoredTypes("type-container", currentPokemonData);
+}
 
-  setBgColor();
-  renderTypes();
+function switchToCardAbout() {
+  document.getElementById("card-data-stats").classList.add("d-none");
+  document.getElementById("card-data-about").classList.remove("d-none");
+  document.getElementById("menu-about").classList.add("active-menu");
+  document.getElementById("menu-stats").classList.remove("active-menu");
+  document
+    .getElementById("menu-stats")
+    .setAttribute("onclick", "switchToCardStats()");
+  document.getElementById("menu-about").onclick = "";
+
+  renderCardAbout();
 }
 
 function renderCardAbout() {
-  let pokemonSpecies = capitalize(currentPokemonData["species"]);
+  let pokemonSpecies = upperCase(currentPokemonData["species"]);
   let pokemonHeight = (currentPokemonData["height"] * 0.1).toFixed(2) + " m";
   let pokemonWeight = (currentPokemonData["weight"] * 0.1).toFixed(2) + " kg";
 
   document.getElementById("pokemon-species").innerHTML = pokemonSpecies;
   document.getElementById("pokemon-height").innerHTML = pokemonHeight;
   document.getElementById("pokemon-weight").innerHTML = pokemonWeight;
-
   renderAbilities();
 }
 
-function renderCardStats() {
-  let pokemonHp = currentPokemonData["stats"][0]["base_stat"];
-  let pokemonAttack = currentPokemonData["stats"][1]["base_stat"];
-  let pokemonDefense = currentPokemonData["stats"][2]["base_stat"];
-  let pokemonSpecialAttack = currentPokemonData["stats"][3]["base_stat"];
-  let pokemonSpecialDefense = currentPokemonData["stats"][4]["base_stat"];
-  let pokemonSpeed = currentPokemonData["stats"][5]["base_stat"];
+function switchToCardStats() {
+  document.getElementById("card-data-about").classList.add("d-none");
+  document.getElementById("card-data-stats").classList.remove("d-none");
+  document.getElementById("menu-stats").classList.add("active-menu");
+  document.getElementById("menu-about").classList.remove("active-menu");
+  document
+    .getElementById("menu-about")
+    .setAttribute("onclick", "switchToCardAbout()");
+  document.getElementById("menu-stats").onclick = "";
 
-  document.getElementById("pokemon-hp").innerHTML = pokemonHp;
-  document.getElementById("pokemon-attack").innerHTML = pokemonAttack;
-  document.getElementById("pokemon-defense").innerHTML = pokemonDefense;
-  document.getElementById("pokemon-special-attack").innerHTML =
-    pokemonSpecialAttack;
-  document.getElementById("pokemon-special-defense").innerHTML =
-    pokemonSpecialDefense;
-  document.getElementById("pokemon-speed").innerHTML = pokemonSpeed;
+  renderCardStats();
 }
 
-function setBgColor() {
-  let mainType = capitalize(currentPokemonData["types"][0]);
-  let index = bgColors.findIndex((i) => i.type == mainType);
+function renderCardStats() {
+  let hp = currentPokemonData["stats"][0]["base_stat"];
+  let attack = currentPokemonData["stats"][1]["base_stat"];
+  let defense = currentPokemonData["stats"][2]["base_stat"];
+  let spAttack = currentPokemonData["stats"][3]["base_stat"];
+  let spDefense = currentPokemonData["stats"][4]["base_stat"];
+  let speed = currentPokemonData["stats"][5]["base_stat"];
 
-  document.getElementById("large-card").style.backgroundColor =
-    bgColors[index]["color"];
+  document.getElementById("pokemon-hp").innerHTML = hp;
+  document.getElementById("pokemon-attack").innerHTML = attack;
+  document.getElementById("pokemon-defense").innerHTML = defense;
+  document.getElementById("pokemon-special-attack").innerHTML = spAttack;
+  document.getElementById("pokemon-special-defense").innerHTML = spDefense;
+  document.getElementById("pokemon-speed").innerHTML = speed;
+
+  renderCardStatsBars(hp, attack, defense, spAttack, spDefense, speed);
+}
+
+function renderCardStatsBars(hp, attack, defense, spAttack, spDefense, speed) {
+  document.getElementById("stat-svg-0").innerHTML = `
+  <line stroke="lightgray" x1="0" y1="0" x2="240" y2="0" />
+  <line stroke="#EE8130" x1="0" y1="0" x2="${hp * 2}" y2="0" />
+  `;
+  document.getElementById("stat-svg-1").innerHTML = `
+  <line stroke="lightgray" x1="0" y1="0" x2="240" y2="0" />
+  <line stroke="#EE8130" x1="0" y1="0" x2="${attack * 2}" y2="0" />
+  `;
+  document.getElementById("stat-svg-2").innerHTML = `
+  <line stroke="lightgray" x1="0" y1="0" x2="240" y2="0" />
+  <line stroke="#EE8130" x1="0" y1="0" x2="${defense * 2}" y2="0" />
+  `;
+  document.getElementById("stat-svg-3").innerHTML = `
+  <line stroke="lightgray" x1="0" y1="0" x2="240" y2="0" />
+  <line stroke="#EE8130" x1="0" y1="0" x2="${spAttack * 2}" y2="0" />
+  `;
+
+  document.getElementById("stat-svg-4").innerHTML = `
+  <line stroke="lightgray" x1="0" y1="0" x2="240" y2="0" />
+  <line stroke="#EE8130" x1="0" y1="0" x2="${spDefense * 2}" y2="0" />
+  `;
+  document.getElementById("stat-svg-5").innerHTML = `
+  <line stroke="lightgray" x1="0" y1="0" x2="240" y2="0" />
+  <line stroke="#EE8130" x1="0" y1="0" x2="${speed * 2}" y2="0" />
+  `;
+
+  for (let i = 0; i <= 5; i++) {
+    let element = document.getElementById("stat-svg-" + i).lastElementChild;
+
+    if (element.getAttribute("x2") >= 100) {
+      element.setAttribute("stroke", "#7AC74C");
+    }
+  }
+}
+
+function setBgColor(targetContainer, sourceData) {
+  let mainType = upperCase(sourceData["types"][0]);
+  let index = typeColors.findIndex((i) => i.type == mainType);
+
+  document.getElementById(targetContainer).style.backgroundColor =
+    typeColors[index]["color"];
 }
 
 function loadTypes(totalTypes) {
@@ -134,19 +236,34 @@ function loadTypes(totalTypes) {
   return types;
 }
 
-function renderTypes() {
-  let types = currentPokemonData["types"];
+function renderTypes(targetContainer, sourceData) {
+  let types = sourceData["types"];
+  document.getElementById(targetContainer).innerHTML = "";
 
   for (let i = 0; i < types.length; i++) {
-    let type = capitalize(types[i]);
-    let index = bgColors.findIndex((a) => a.type == type);
+    let type = upperCase(types[i]);
 
-    document.getElementById("type-container").innerHTML += `
-      <span id="type${i}" class="type">${type}</span>
+    document.getElementById(targetContainer).innerHTML += `
+      <span id="${targetContainer}-type${i}" class="type">${type}</span>
+    `;
+  }
+}
+
+function renderColoredTypes(targetContainer, sourceData) {
+  let types = sourceData["types"];
+  document.getElementById(targetContainer).innerHTML = "";
+
+  for (let i = 0; i < types.length; i++) {
+    let type = upperCase(types[i]);
+    let index = typeColors.findIndex((a) => a.type == type);
+
+    document.getElementById(targetContainer).innerHTML += `
+      <span id="${targetContainer}-type${i}" class="type">${type}</span>
     `;
 
-    document.getElementById(`type${i}`).style.backgroundColor =
-      bgColors[index]["color"];
+    document.getElementById(
+      `${targetContainer}-type${i}`
+    ).style.backgroundColor = typeColors[index]["color"];
   }
 }
 
@@ -162,8 +279,10 @@ function loadAbilities(totalAbilities) {
 
 function renderAbilities() {
   let abilities = currentPokemonData["abilities"];
+  document.getElementById("pokemon-abilities").innerHTML = "";
+
   for (let i = 0; i < abilities.length; i++) {
-    let ability = capitalize(abilities[i]);
+    let ability = upperCase(abilities[i]);
 
     if (i != abilities.length - 1) {
       document.getElementById("pokemon-abilities").innerHTML += ability + ", ";
@@ -173,8 +292,31 @@ function renderAbilities() {
   }
 }
 
-function capitalize(word) {
+function showNextPokemon() {
+  if (currentPokemon < numberOfPokemon) currentPokemon++;
+  renderPokemonCard();
+}
+
+function showPreviousPokemon() {
+  if (currentPokemon > 1) currentPokemon--;
+  renderPokemonCard();
+}
+
+function disableButtons() {
+  if (currentPokemon == 1) document.getElementById("back-btn").disabled = true;
+  else document.getElementById("back-btn").disabled = false;
+
+  if (currentPokemon == numberOfPokemon)
+    document.getElementById("next-btn").disabled = true;
+  else document.getElementById("next-btn").disabled = false;
+}
+
+function upperCase(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function lowerCase(word) {
+  return word.charAt(0).toLowerCase() + word.slice(1);
 }
 
 function saveToLocalStorage(key, array) {
